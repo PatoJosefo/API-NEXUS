a1cfa1a1cac381ec79f4cd2b71b996b6997f8ba3
Talita Marques <talitadelania123@gmail.com>
2025-05-04 19:54:39 -0300

    Adição de arquivos de Análise

---
+++ src/backend/analise.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def realizar_analise(dados, df_municipios, df_ncm_sh, ano, atividade, tipo, mes_num, municipio):
+    print(f"Parâmetros Selecionados:")
+    print(f"- Ano: {ano}")
+    print(f"- Atividade: {atividade}")
+    print(f"- Filtro: {tipo}")
+    print(f"- Mês: {'Todos' if mes_num is None else mes_num}")
+    print(f"- Município: {municipio}\n")
+
+    df_base = dados[ano]['IMP'] if atividade == 'Importação' else dados[ano]['EXP']
+    print(df_base.head())
+    df = df_base.copy()
+
+    df = df[df['KG_LIQUIDO'] > 0]
+    df['VALOR_AGREGADO'] = df['VL_FOB'] / df['KG_LIQUIDO']
+    df = df.merge(df_municipios, on='CO_MUN', how='left')
+    df = df.merge(df_ncm_sh, on='SH4', how='left')
+
+    if mes_num is not None:
+        df = df[df['CO_MES'] == mes_num]
+    df = df[df['NO_MUN'] == municipio]
+
+    match tipo:
+        case 'Valor Agregado':
+            resultado = df.groupby(['NO_MUN', 'SG_UF_MUN', 'NO_SH4_POR'])['VALOR_AGREGADO'].mean().reset_index()
+            resultado = resultado.sort_values(by='VALOR_AGREGADO', ascending=False).head(5)
+        case 'SH4':
+            resultado = df['NO_SH4_POR'].value_counts().head(5).reset_index()
+            resultado.columns = ['NO_SH4_POR', 'Contagem']
+        case 'KG_LIQUIDO':
+            resultado = df.groupby('NO_SH4_POR')['KG_LIQUIDO'].sum().sort_values(ascending=False).head(5).reset_index()
+        case 'Valor FOB':
+            resultado = df.groupby('NO_SH4_POR')['VL_FOB'].sum().sort_values(ascending=False).head(5).reset_index()
+        case _:
+            resultado = pd.DataFrame({'Mensagem': ['Tipo de análise não reconhecido']})
+
+    #display(resultado)
+    return resultado
---
+++ src/backend/leitura_dataset.py
@@ -0,0 +1,66 @@
+from google.colab import drive
+import pandas as pd
+
+__path = '/content/drive/MyDrive/Colab Notebooks/API1S/base_dados_por_municipio'
+
+def autenticacao_google():
+    drive.mount('/content/drive')
+
+
+def __leitura_arq_cod_nome(lista_nomes_arq): #[]'ncm', 'co_sh']
+  lista_arq = []
+
+  for arq in lista_nomes_arq:
+    path = f'{__path}/{arq}.csv'
+    df = pd.read_csv(path, delimiter=';', on_bad_lines='skip', encoding='latin-1')
+    df.columns = df.columns.str.strip()
+
+    if 'SG_UF' in df.columns:
+      df = df[df['SG_UF'] == 'SP']
+
+    lista_arq.append(df)
+
+  return lista_arq
+
+
+def __leitura_arq_mun(tipo, ano):
+  path = f'{__path}/{tipo}_{ano}_MUN.csv'
+  df_mun = pd.read_csv(path, delimiter=';', on_bad_lines='skip', encoding='latin-1', usecols='''CO_MES SH4 CO_PAIS SG_UF_MUN CO_MUN KG_LIQUIDO VL_FOB'''.split())
+  df_mun.columns = df_mun.columns.str.strip()
+  df_sp = df_mun[df_mun['SG_UF_MUN'] == 'SP']
+
+  return df_sp
+
+
+def __carregar_dados_mun():
+  dados = {}
+
+  for ano in range(2019, 2025):
+    dados[ano] = {'EXP': __leitura_arq_mun('EXP', ano, __path), 'IMP': __leitura_arq_mun('IMP', ano, __path)}
+
+  return dados
+
+
+def pegar_dados():
+  dados = __carregar_dados_mun()
+
+  #carregamento dos arquivos de códigos e correspondências:
+  nome_arq = ['UF_MUN', 'PAIS', 'NCM_SH']
+  lista_arquivos = __leitura_arq_cod_nome(nome_arq)
+
+  lista_arquivos[0].rename(columns={'CO_MUN_GEO': 'CO_MUN'}, inplace=True)
+  lista_arquivos[2].rename(columns={'CO_SH4': 'SH4'}, inplace=True)
+
+  df_municipios = lista_arquivos[0][['CO_MUN', 'NO_MUN']]
+  df_ncm_sh = lista_arquivos[2][['SH4', 'NO_SH4_POR']]
+
+  df_mun_nomes = sorted(df_municipios['NO_MUN'].unique().tolist())
+  df_ncm_sh_nomes = sorted(df_ncm_sh['NO_SH4_POR'].unique().tolist())
+
+  util_data = {
+    "df_municipios": df_municipios,
+    "df_ncm_sh": df_ncm_sh,
+    "df_mun_nomes": df_mun_nomes,
+    "df_ncm_sh_nomes": df_ncm_sh_nomes,
+  }
+  return dados, util_data
